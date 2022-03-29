eb92e3882be66a6c896b3783893f783d707c1488
Joshualwh <joshualing0711@gmail.com>
2022-03-29 17:11:51 +0800

    Confirmed dict creation

--- trimesh_testing/trimesh_testing.py
+++ trimesh_testing/trimesh_testing.py
@@ -14,7 +14,6 @@ import numpy as np
 import time
 import fcl
 
-
 def precalculate_surface_point_cloud() :
 
     file = "osr_description/urdf/denso_vs060.urdf"
@@ -84,61 +83,61 @@ def trimesh_collision () :
     # print (collision_data)
     print (collision_name)
 
-# def voxel_collision_status () :
+def voxel_collision_status_1 () :
 
-#     #Create Coordinate System for Translation
-#     query_points_world_list = torch.zeros((64000, 4), dtype=dtype, device=dev)
-#     x_count = 0
-#     y_count = 0
-#     z_count = 0
-#     for not_sure_index in range (len(query_points_world_list)) :
-#         query_points_world_list[not_sure_index][0] = (0.04 * x_count) - 0.78
-#         query_points_world_list[not_sure_index][1] = (0.04 * y_count) - 0.78
-#         query_points_world_list[not_sure_index][2] = (0.04 * z_count) - 0.78
-#         if z_count == 40:
-#             y_count +=1
-#             z_count = 0
-#             if y_count == 40 :
-#                 x_count +=1
-#                 y_count = 0
-#         z_count +=1
-#     # print (query_points_world_list)
+    #Create Coordinate System for Translation
+    query_points_world_list = torch.zeros((64000, 4), dtype=dtype, device=dev)
+    x_count = 0
+    y_count = 0
+    z_count = 0
+    for not_sure_index in range (len(query_points_world_list)) :
+        query_points_world_list[not_sure_index][0] = (0.04 * x_count) - 0.78
+        query_points_world_list[not_sure_index][1] = (0.04 * y_count) - 0.78
+        query_points_world_list[not_sure_index][2] = (0.04 * z_count) - 0.78
+        if z_count == 40:
+            y_count +=1
+            z_count = 0
+            if y_count == 40 :
+                x_count +=1
+                y_count = 0
+        z_count +=1
+    # print (query_points_world_list)
 
-#     # Transformation Matrix (Translation)
-#     voxel_collision_manager = CollisionManager()
-#     voxel_transform_identity = torch.zeros((4, 4), dtype=dtype, device=dev)
-#     for i in range (4) :
-#         voxel_transform_identity[i][i] = 1
+    # Transformation Matrix (Translation)
+    voxel_collision_manager = CollisionManager()
+    voxel_transform_identity = torch.zeros((4, 4), dtype=dtype, device=dev)
+    for i in range (4) :
+        voxel_transform_identity[i][i] = 1
     
-#     # Creation of 64000 Voxel Mesh, saved in voxel_collision_manager
-#     for count in range (len(query_points_world_list)) :
-#     # for count in range (64000) :
-#         for i in range (3) : 
-#             voxel_transform_identity [i][3] = query_points_world_list[count][i]
-#             # voxel_transform_identity [1][3] = query_points_world_list[0][1]
-#             # voxel_transform_identity [2][3] = query_points_world_list[0][2]
-#         voxel_box = box(extents=(0.04, 0.04, 0.04), transform=voxel_transform_identity)
-#         # trimesh.points.PointCloud.show(voxel_box)
-#         voxel_index = 'voxel_' + str(count)
-#         voxel_collision_manager.add_object(voxel_index, voxel_box)
+    # Creation of 64000 Voxel Mesh, saved in voxel_collision_manager
+    for count in range (len(query_points_world_list)) :
+    # for count in range (64000) :
+        for i in range (3) : 
+            voxel_transform_identity [i][3] = query_points_world_list[count][i]
+            # voxel_transform_identity [1][3] = query_points_world_list[0][1]
+            # voxel_transform_identity [2][3] = query_points_world_list[0][2]
+        voxel_box = box(extents=(0.04, 0.04, 0.04), transform=voxel_transform_identity)
+        # trimesh.points.PointCloud.show(voxel_box)
+        voxel_index = 'voxel_' + str(count)
+        voxel_collision_manager.add_object(voxel_index, voxel_box)
 
-#     # names = voxel_collision_manager._names
-#     # print (names)
+    # names = voxel_collision_manager._names
+    # print (names)
 
-#     # Import Robot Mesh to calculate minimum distance between each voxel to mesh
-#     file = "osr_description/urdf/denso_vs060.urdf"
-#     robot = URDF.from_xml_file(file)
-#     links = robot.links
-#     n_links = len(links)
-#     n_links = 1
+    # Import Robot Mesh to calculate minimum distance between each voxel to mesh
+    file = "osr_description/urdf/denso_vs060.urdf"
+    robot = URDF.from_xml_file(file)
+    links = robot.links
+    n_links = len(links)
+    n_links = 1
 
-#     for i in range (n_links): 
-#         robot_mesh_filename = robot.links[0].collision.geometry.filename
-#         robot_link = robot_mesh_filename.replace('package://', '')
-#         mesh = trimesh.load_mesh(robot_link)
-#         print ('Distancing...' + str(i))
-#         voxel_distance, voxel_name = voxel_collision_manager.in_collision_single(mesh, transform=None, return_names=True, return_data=False)
-#         print(voxel_distance, voxel_name)
+    for i in range (n_links): 
+        robot_mesh_filename = robot.links[i].collision.geometry.filename
+        robot_link = robot_mesh_filename.replace('package://', '')
+        mesh = trimesh.load_mesh(robot_link)
+        print ('Distancing...' + str(i))
+        collision_status, voxel_name = voxel_collision_manager.in_collision_single(mesh, transform=None, return_names=True, return_data=False)
+        print(collision_status, voxel_name)
 
 def voxel_distance_calculation () :
 
@@ -270,10 +269,11 @@ if __name__ == "__main__":
     saved_cloud = np.empty(7, dtype=object)
     # precalculate_surface_point_cloud()
     # trimesh_collision()
-    # voxel_collision_status()
-    trimesh_collision_status = pd.DataFrame()
-    trimesh_collision_status = voxel_collision_status()
-    trimesh_collision_status.to_csv('trimesh_collision_status.csv')
+    # # voxel_collision_status()
+    # trimesh_collision_status = pd.DataFrame()
+    # trimesh_collision_status = voxel_collision_status()
+    # trimesh_collision_status.to_csv('trimesh_collision_status.csv')
+    voxel_collision_status_1()
     # voxel_distance_calculation()
     # trimesh_sdf = pd.DataFrame()
     # trimesh_sdf = voxel_distance_calculation()
